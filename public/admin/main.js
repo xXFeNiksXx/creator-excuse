a79994a74af19209ba608a914edb04e24ab27d1f
Sviatoslav <sviatos1706@gmail.com>
2024-07-05 13:36:25 +0300

    add function adding new excuse

--- public/admin/main.js
+++ public/admin/main.js
@@ -1,22 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('addBtn').addEventListener('click', function() {
-        const excuseText = document.getElementById('excuseText').value;
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('addBtn').addEventListener('click', function() {
+//         const excuseText = document.getElementById('excuseText').value;
 
-        fetch('http://localhost:3000/add-excuse', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text: excuseText })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) {
-                alert('Excuse added successfully!');
-            } else {
-                alert('Error: ' + data.error);
-            }
-        })
-        .catch(error => console.error('Error:', error));
+//         fetch('http://localhost:5000/add-excuse', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ text: excuseText })
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.message) {
+//                 alert('Excuse added successfully!');
+//             } else {
+//                 alert('Error: ' + data.error);
+//             }
+//         })
+//         .catch(error => console.error('Error:', error));
+//     });
+// });
+$('#addBtn').click(function(){
+    axios.post('http://localhost:5000/add-excuse', {
+        mesexcuse:$('#excuseText').val(),
+        avtor:$('#avtor').val()
+    })
+    .then(response => {
+      console.log( response.data);
+    })
+    .catch(error => {
+      console.error('Помилка при відправці даних:', error);
     });
-});
+})
--- public/main.js
+++ public/main.js
@@ -2,11 +2,11 @@ function getAllExcuses(){
     axios.get('http://localhost:5000/excuse')
     .then(res=>{
         console.log(res.data);
-        for(let el of res.data){
-            $('.excuseBox').append(`
-                <div class="excuse">${el.mesexcuse}</div>
-            `)
-        }
+        // for(let el of res.data){
+        //     $('.excuseBox').append(`
+        //         <div class="excuse">${el.mesexcuse}</div>
+        //     `)
+        // }
     })
 }
 getAllExcuses()
